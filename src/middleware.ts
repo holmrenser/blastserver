5be5516eb7a7cf79f9d94e4344a3117179470805
Rens Holmer <8225367+holmrenser@users.noreply.github.com>
2023-07-06 11:44:24 +0200

    more cors experiments

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    // retrieve the current response
+    const response = NextResponse.next()
+
+    if (request.nextUrl.pathname.startsWith("/api")) {
+        response.headers.append("Access-Control-Allow-Origin", "*")
+        response.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+        response.headers.append(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+    }
+    return response
+}
