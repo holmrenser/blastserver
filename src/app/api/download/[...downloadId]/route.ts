565263cc163f608843afc2c9bcd2e82e5bfc58d6
Rens Holmer <8225367+holmrenser@users.noreply.github.com>
2023-09-12 20:47:33 +0200

    downloads, db schema prep

---
+++ src/app/api/download/[...downloadId]/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function GET(_: NextRequest, context: { params: { downloadId: string[]}}) {
+  const { params: { downloadId }} = context;
+  console.log(`Requested download job ${downloadId}`);
+
+  let job;
+  try {
+    job = await prisma.blastjob.findFirst({ where: { id: downloadId[0] }});
+  } catch (err) {
+    console.error(err);
+  }
+  // const formattedResults = job?.results ? await formatResults(job.results) : null;
+
+  return NextResponse.json({...job })
+}
---
+++ src/app/api/download/route.ts
@@ -0,0 +1,37 @@
+import hash from 'object-hash';
+import { NextResponse, NextRequest } from 'next/server';
+
+import { downloadQueue } from '../queue';
+import prisma from '../database';
+
+export const dynamic = 'force-dynamic';
+
+export async function POST(request: NextRequest) {
+  // const { sequenceIds }: { sequenceIds: string[] } = await request.json();
+  const params = await request.json();
+  const { sequenceIds, database }: { sequenceIds: string[], database: string } = params;
+  if (!Array.isArray(sequenceIds)) {
+    return NextResponse.json({ error: 'Must provide an array of sequence identifiers'}, { status: 500})
+  }
+
+  if (sequenceIds.length > 500) {
+    return NextResponse.json({ error: 'Cannot download more than 500 sequences'}, { status: 500 })
+  }
+
+  sequenceIds.sort();
+  const jobId = hash(sequenceIds);
+  console.log(`Download request ${jobId}`)
+  const existingJob = await prisma.download.findFirst({ where: { id: jobId }});
+  if (!existingJob) {
+    prisma.download.create({
+      data: {
+        id: jobId,
+        sequenceIds,
+        submitted: new Date(),
+      }
+    }).then(() => downloadQueue.add('download', { sequenceIds, database }, { jobId }))
+  } else {
+    console.log(`Found existing download job: ${existingJob.id}`)
+  }
+  return NextResponse.json({ jobId })
+}
