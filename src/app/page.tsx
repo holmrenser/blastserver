55c0951175172ac0349f78942f83e05bd39543eb
Rens Holmer <8225367+holmrenser@users.noreply.github.com>
2024-07-16 13:11:10 +0200

    reverting some christmas fun and updating dependency versions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,177 @@
-'use client'
+"use client";
 
-import { useContext } from 'react';
-import Link from 'next/link';
+import { useContext } from "react";
+import Link from "next/link";
 
-import { ThemeContext } from './themecontext';
-import type { Theme } from './themecontext';
+import { ThemeContext } from "./themecontext";
+import type { Theme } from "./themecontext";
 
-import './page.scss';
+import "./page.scss";
 
-type Moltype = 'nucleotide' | 'protein';
+type Moltype = "nucleotide" | "protein";
 
-function QueryTargetTags({ query, target, theme }: {query: Moltype, target: Moltype, theme: Theme }): JSX.Element {
-  return <div className='field is-grouped is-grouped-multiline'>
-    <div className='control'>
-    <div className='tags has-addons'>
-      <span className={`tag ${theme === 'dark' ? 'is-dark' : 'is-white'}`}>Query</span>
-      <span className={`tag ${query === 'nucleotide' ? 'is-primary' : 'is-info'}`}>{query}</span>
+function QueryTargetTags({
+  query,
+  target,
+  theme,
+}: {
+  query: Moltype;
+  target: Moltype;
+  theme: Theme;
+}): JSX.Element {
+  return (
+    <div className="field is-grouped is-grouped-multiline">
+      <div className="control">
+        <div className="tags has-addons">
+          <span className={`tag ${theme === "dark" ? "is-dark" : "is-white"}`}>
+            Query
+          </span>
+          <span
+            className={`tag ${
+              query === "nucleotide" ? "is-primary" : "is-info"
+            }`}
+          >
+            {query}
+          </span>
+        </div>
+      </div>
+      <div className="control">
+        <div className="tags has-addons">
+          <span className={`tag ${theme === "dark" ? "is-dark" : "is-white"}`}>
+            Target
+          </span>
+          <span
+            className={`tag ${
+              target === "nucleotide" ? "is-primary" : "is-info"
+            }`}
+          >
+            {target}
+          </span>
+        </div>
+      </div>
     </div>
-    </div>
-    <div className='control'>
-    <div className='tags has-addons'>
-      <span className={`tag ${theme === 'dark' ? 'is-dark' : 'is-white'}`}>Target</span>
-      <span className={`tag ${target === 'nucleotide' ? 'is-primary' : 'is-info'}`}>{target}</span>
-    </div>
-    </div>
-  </div>
+  );
 }
 
 export default function HomePage() {
-  // const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
   const { theme } = useContext(ThemeContext);
   return (
-    <section className={`section ${theme === 'dark' ? 'has-background-dark has-text-light' : ''}`}>
-      <div className='container is-fullhd'>
-        <section className='hero is-fullheight'>
-          <div className='hero-body' style={{display: 'unset'}}>
-            <p className={`title ${theme === 'dark' ? 'has-text-light' : ''}`}>BLAST SERVER</p>
-            <p className={`subtitle ${theme === 'dark' ? 'has-text-light' : ''}`}>WUR Bioinformatics Group</p>
-            <div className='columns'>
-              <div className={`column ${theme === 'dark' ? 'has-background-grey-dark' : 'has-background-light'}`}>
-                <QueryTargetTags query='protein' target='protein' theme={theme}/>
+    <section
+      className={`section ${
+        theme === "dark" ? "has-background-dark has-text-light" : ""
+      }`}
+    >
+      <div className="container is-fullhd">
+        <section className="hero is-fullheight">
+          <div className="hero-body" style={{ display: "unset" }}>
+            <p className={`title ${theme === "dark" ? "has-text-light" : ""}`}>
+              BLAST SERVER
+            </p>
+            <p
+              className={`subtitle ${theme === "dark" ? "has-text-light" : ""}`}
+            >
+              WUR Bioinformatics Group
+            </p>
+            <div className="columns">
+              <div
+                className={`column ${
+                  theme === "dark"
+                    ? "has-background-grey-dark"
+                    : "has-background-light"
+                }`}
+              >
+                <QueryTargetTags
+                  query="protein"
+                  target="protein"
+                  theme={theme}
+                />
                 <Link
-                  className='button is-large is-fullwidth'
-                  href='/blastp'
-                  title='Search a protein database with a protein query'
+                  className="button is-large is-fullwidth"
+                  href="/blastp"
+                  title="Search a protein database with a protein query"
                 >
                   blastp
                 </Link>
               </div>
-              <div className={`column ${theme === 'dark' ? 'has-background-grey-dark' : 'has-background-light'}`}>
-                <QueryTargetTags query='nucleotide' target='nucleotide' theme={theme} />
+              <div
+                className={`column ${
+                  theme === "dark"
+                    ? "has-background-grey-dark"
+                    : "has-background-light"
+                }`}
+              >
+                <QueryTargetTags
+                  query="nucleotide"
+                  target="nucleotide"
+                  theme={theme}
+                />
                 <Link
-                      //@ts-ignore
-                      disabled
-                      className='button is-large is-fullwidth'
-                      href='/blastn'
-                      title='Search a nucleotide database with a nucleotide query'
-                      style={{pointerEvents: 'none'}}
-                    >
-                      blastn
-                    </Link>
-                    <Link
-                      //@ts-ignore
-                      disabled
-                      className='button is-large is-fullwidth'
-                      href='/tblastx'
-                      title='Search a translated nucleotide database with a translated nucleotide query'
-                      style={{pointerEvents: 'none'}}
-                    >
-                      tblastx
-                    </Link>
+                  //@ts-ignore
+                  disabled
+                  className="button is-large is-fullwidth"
+                  href="/blastn"
+                  title="Search a nucleotide database with a nucleotide query"
+                  style={{ pointerEvents: "none" }}
+                >
+                  blastn
+                </Link>
+                <Link
+                  //@ts-ignore
+                  disabled
+                  className="button is-large is-fullwidth"
+                  href="/tblastx"
+                  title="Search a translated nucleotide database with a translated nucleotide query"
+                  style={{ pointerEvents: "none" }}
+                >
+                  tblastx
+                </Link>
               </div>
-              <div className={`column ${theme === 'dark' ? 'has-background-grey-dark' : 'has-background-light'}`}>
-                <QueryTargetTags query='nucleotide' target='protein' theme={theme} />
-                  <Link
-                    //@ts-ignore
-                    disabled
-                    className='button is-large is-fullwidth'
-                    href='/blastx'
-                    title='Search a protein database with a translated nucleotide query'
-                    style={{pointerEvents: 'none'}}
-                  >
-                    blastx
-                  </Link>
+              <div
+                className={`column ${
+                  theme === "dark"
+                    ? "has-background-grey-dark"
+                    : "has-background-light"
+                }`}
+              >
+                <QueryTargetTags
+                  query="nucleotide"
+                  target="protein"
+                  theme={theme}
+                />
+                <Link
+                  //@ts-ignore
+                  disabled
+                  className="button is-large is-fullwidth"
+                  href="/blastx"
+                  title="Search a protein database with a translated nucleotide query"
+                  style={{ pointerEvents: "none" }}
+                >
+                  blastx
+                </Link>
               </div>
-              <div className={`column ${theme === 'dark' ? 'has-background-grey-dark' : 'has-background-light'}`}>
-                <QueryTargetTags query='protein' target='nucleotide' theme={theme} />
+              <div
+                className={`column ${
+                  theme === "dark"
+                    ? "has-background-grey-dark"
+                    : "has-background-light"
+                }`}
+              >
+                <QueryTargetTags
+                  query="protein"
+                  target="nucleotide"
+                  theme={theme}
+                />
                 <Link
                   // @ts-ignore
                   disabled
-                  className='button is-large is-fullwidth'
-                  href='/tblastn'
-                  title='Search a translated nucleotide database with a protein query'
-                  style={{pointerEvents: 'none'}}
+                  className="button is-large is-fullwidth"
+                  href="/tblastn"
+                  title="Search a translated nucleotide database with a protein query"
+                  style={{ pointerEvents: "none" }}
                 >
                   tblastn
                 </Link>
               </div>
-              
             </div>
             {/*
             <table className={`table flavour-table ${theme}`}>
@@ -180,5 +254,5 @@ export default function HomePage() {
         </section>
       </div>
     </section>
-  )
+  );
 }
